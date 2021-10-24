f541e64a2312d01e4d9557bb146bad408b05c7b9
delfiansteel <delfiansteel7.ds@gmail.com>
2021-10-24 19:54:32 +0700

    Menambahkan file pyhton1.py

---
+++ pyhton1.py
@@ -0,0 +1,19 @@
+##Input Data##
+name = "Delfian Ruly Havatilla."
+nickname = "Del or Pian."
+nim ="312110161."
+born = "Bekasi"
+old = "18"
+address = "Jalan Kp. Tangsi Blok 03 No.06, Sukadanau, Kec. Cikarang Bar., Bekasi, Jawa Barat 17530."
+numb = "081314144467"
+
+##Output Data##
+print ("Assalammu'alaikum..")
+
+print ()
+
+print ("Let me introduce my self. My name is " +name,"But you can call me " +nickname,"My NIM is " +nim,"I was born in " + born,"and I am "+old,"years old.","I am very glad if you want to invite my house in " +address,"So don't forget to call me before with the number " +numb)
+
+print ()
+print ("Thank you.")
+##Output done##
